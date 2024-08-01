3e8bccc0b3d1ec453071d71cb1c7a7493e7ea6b9
ruslanhal <r.halushka.work@gmail.com>
2024-08-01 17:03:07 +0300

    change colour of skeleton

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,22 @@ export default {
   theme: {
     extend: {
       animation: {
-        blink: 'blink 1.5s infinite ease-in-out',
-        blinkTitle: 'blinkTitle 1.5s infinite ease-in-out',
-        blinkSpan: 'blinkSpan 1.5s infinite ease-in-out'
+        blink: "blink 1.5s infinite ease-in-out",
+        blinkTitle: "blinkTitle 1.5s infinite ease-in-out",
+        blinkSpan: "blinkSpan 1.5s infinite ease-in-out",
       },
       keyframes: {
         blink: {
-          '0%, 100%': { backgroundColor: '#A9A9A9' },
-          '50%': { backgroundColor: '#DCDCDC' },
+          "0%, 100%": {backgroundColor: "#ECECEC"},
+          "50%": {backgroundColor: "#F9F9F9"},
         },
         blinkTitle: {
-          '0%, 100%': { backgroundColor: '#D3D3D3' },
-          '50%': { backgroundColor: '#616161' },
+          "0%, 100%": {backgroundColor: "#D3D3D3"},
+          "50%": {backgroundColor: "#616161"},
         },
         blinkSpan: {
-          '0%, 100%': { backgroundColor: '#E8E9EB' },
-          '50%': { backgroundColor: '#9e9e9e' },
+          "0%, 100%": {backgroundColor: "#E8E9EB"},
+          "50%": {backgroundColor: "#9e9e9e"},
         },
       },
     },
